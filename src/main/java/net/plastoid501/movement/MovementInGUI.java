c3b9bd479a7fd188baef7e7ff2b9b9c8dc6bebb0
plastoid501 <plastoid501@gmail.com>
2024-07-17 20:13:46 +0900

    mc1.16.5

--- src/main/java/net/plastoid501/movement/MovementInGUI.java
+++ src/main/java/net/plastoid501/movement/MovementInGUI.java
@@ -1,15 +1,15 @@
 package net.plastoid501.movement;
 
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class MovementInGUI implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("movement-in-gui");
+	public static final Logger LOGGER = LogManager.getLogger("movement-in-gui");
 
 	@Override
 	public void onInitialize() {
