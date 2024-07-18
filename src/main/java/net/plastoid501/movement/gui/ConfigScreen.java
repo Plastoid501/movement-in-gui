fea68dedbded14493b033f71b4456ef603428954
plastoid501 <plastoid501@gmail.com>
2024-07-18 20:31:48 +0900

    mc1.20.1

--- src/main/java/net/plastoid501/movement/gui/ConfigScreen.java
+++ src/main/java/net/plastoid501/movement/gui/ConfigScreen.java
@@ -27,9 +27,9 @@ public class ConfigScreen extends Screen {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        super.render(context, mouseX, mouseY, delta);
         this.configList.render(context, mouseX, mouseY, delta);
         context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 8, 0xFFFFFF);
+        super.render(context, mouseX, mouseY, delta);
     }
 
     @Override
