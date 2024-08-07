a50221887ceae36673a825fcd0f406835f4f1371
plastoid501 <plastoid501@gmail.com>
2024-08-07 15:04:48 +0900

    fixed https://github.com/Plastoid501/movement-in-gui/issues/2

--- src/main/java/net/plastoid501/movement/config/Configs.java
+++ src/main/java/net/plastoid501/movement/config/Configs.java
@@ -11,14 +11,17 @@ public class Configs {
 
     public static ToggleConfig modEnable = new ToggleConfig("modEnable", "If true, this mod is enable", true);
     public static ToggleConfig inCreative = new ToggleConfig("inCreative", "If true, this mod is enable when creative mode.", true);
+    public static ToggleConfig isAnvil = new ToggleConfig("inCreative", "If true, this mod is enable when open anvil screen.", true);
 
     public static ModConfig config;
 
     static {
         toggles.put(modEnable.getId(), modEnable);
         toggles.put(inCreative.getId(), inCreative);
+        toggles.put(isAnvil.getId(), isAnvil);
         jToggles.put(modEnable.getId(), new JToggleConfig(modEnable.isEnable()));
         jToggles.put(inCreative.getId(), new JToggleConfig(inCreative.isEnable()));
+        jToggles.put(isAnvil.getId(), new JToggleConfig(isAnvil.isEnable()));
 
         config = new ModConfig(jToggles);
 
