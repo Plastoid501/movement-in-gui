1dc75a9ad26078f7ce93e529d308b8d03c704751
plastoid501 <plastoid501@gmail.com>
2025-02-17 17:23:55 +0900

    add isMultiPlayer

--- src/main/java/net/plastoid501/movement/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/net/plastoid501/movement/mixin/ClientPlayerEntityMixin.java
@@ -3,6 +3,9 @@ package net.plastoid501.movement.mixin;
 import com.terraformersmc.modmenu.gui.ModsScreen;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.*;
+import net.minecraft.client.gui.screen.ingame.AnvilScreen;
+import net.minecraft.client.gui.screen.ingame.BookEditScreen;
+import net.minecraft.client.gui.screen.ingame.SignEditScreen;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
 import net.minecraft.client.gui.screen.option.OptionsScreen;
 import net.minecraft.client.gui.screen.pack.PackScreen;
@@ -26,6 +29,9 @@ public class ClientPlayerEntityMixin {
         }
         MinecraftClient client = MinecraftClient.getInstance();
         if (client.currentScreen == null ||
+                client.currentScreen instanceof BookEditScreen ||
+                client.currentScreen instanceof SignEditScreen ||
+                (client.currentScreen instanceof AnvilScreen && !Configs.isAnvil.isEnable()) ||
                 client.currentScreen instanceof ChatScreen ||
                 client.currentScreen instanceof GameOptionsScreen ||
                 client.currentScreen instanceof GameMenuScreen ||
@@ -42,6 +48,9 @@ public class ClientPlayerEntityMixin {
             return false;
         }
         if (!Configs.inCreative.isEnable() && client.player != null && client.player.isCreative()) {
+            return false;
+        }
+        if (!Configs.isMultiplayer.isEnable() && !client.isInSingleplayer()) {
             return false;
         }
         InputUtil.Key key = ((IKeyBindingMixin) instance).getBoundKey();
